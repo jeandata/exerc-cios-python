3d9a163fd738d4fefa388c22f76f28519022cda6
Jean Alves <jean_prog@hotmail.com>
2024-08-19 18:58:00 -0300

    Remoção de conteúdos
    Mudança da Pasta

--- Sabor Express/while.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um numero: "))
-valor = 1
-while valor <= 10:
-    resultado = numero * valor
-    print(f"{numero} x {valor} = {resultado}")
-    valor += 1
-print("Valores criados")
